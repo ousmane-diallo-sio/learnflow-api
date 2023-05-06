3c3f70f8e94ad0016a5ab0bea10e367f09e88804
Aaldn <aaldn.dev@gmail.com>
2023-05-06 15:56:04 +0200

    feat: add document model

---
+++ src/models/document.ts
@@ -0,0 +1,27 @@
+import Joi from "joi";
+import mongoose from "mongoose";
+import { DocumentTypeValidationSchema } from "./documentType";
+
+export interface Document {
+  uploadUrl: string
+  documentType: DocumentType
+}
+
+export const DocumentValidationSchema = Joi.object({
+  uploadUrl: Joi.string().min(3).max(2048).required(),
+  documentType: DocumentTypeValidationSchema.required(),
+})
+
+export const DocumentSchema = new mongoose.Schema({
+  uploadUrl: {
+      type: String,
+      required: true,
+      unique: true,
+  },
+  documentType: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'DocumentType',
+  },
+})
+
+export const Document = mongoose.model('Document', DocumentSchema)
