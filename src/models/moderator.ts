9a958a6f9f212c3f1234dc38d85d4dfc42cd51ec
Aaldn <aaldn.dev@gmail.com>
2023-05-06 15:57:21 +0200

    feat: add moderator model

---
+++ src/models/moderator.ts
@@ -0,0 +1,32 @@
+import Joi from "joi";
+import mongoose from "mongoose";
+
+export interface Moderator {
+  firstName: string
+  lastName: string
+  email: string
+}
+
+export const ModeratorValidationSchema = Joi.object({
+  firstName: Joi.string().min(1).max(50).required(),
+  lastName: Joi.string().min(1).max(50).required(),
+  email: Joi.string().email().required(),
+})
+
+export const ModeratorSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+})
+
+export const Moderator = mongoose.model('Moderator', ModeratorSchema)
