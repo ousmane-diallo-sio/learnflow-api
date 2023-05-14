7f7b71b8bb0897bb01bc445492cfe9670b4eeffa
Aaldn <aaldn.dev@gmail.com>
2023-05-14 23:09:39 +0200

    feat: add password validation

--- src/validators/moderator.ts
+++ src/validators/moderator.ts
@@ -1,9 +1,11 @@
 import Joi from "joi";
+import { PasswordValidationSchema } from './password';
 
 const ModeratorValidationSchema = Joi.object({
   firstName: Joi.string().min(1).max(50).required(),
   lastName: Joi.string().min(1).max(50).required(),
   email: Joi.string().email().required(),
+  password: PasswordValidationSchema.required(),
 })
 
 export default ModeratorValidationSchema
